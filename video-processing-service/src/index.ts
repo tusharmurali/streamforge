f7b51f8e674cd90eee9411e82da23eabfcca70b0
Tushar Muralidharan <u7253634@anu.edu.au>
2025-08-18 18:28:55 +1000

    Process videos locally using child_process

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,39 @@
 import express from 'express';
+import { spawn } from 'child_process';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.post('/process-video', (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
 
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send('Bad request: missing file path');
+    }
+
+    const ffmpeg = spawn('ffmpeg', [
+        '-i', inputFilePath,
+        '-vf', 'scale=-1:360',
+        outputFilePath
+    ]);
+
+    let stderrLog = '';
+
+    ffmpeg.stderr.on('data', (data) => {
+        stderrLog += data.toString();
+    });
+
+    ffmpeg.on('close', (code) => {
+        if (code === 0) {
+            res.status(200).send('Processing finised successfully');
+        } else {
+            res.status(500).send('Internal server error: ' + (stderrLog || `FFmpeg exited with code ${code}`))
+        }
+    });
+})
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Video processing service listening at http://localhost:${port}`);
 });
